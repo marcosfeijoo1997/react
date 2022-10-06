dc47673455ecd815a13f833424f1077cd72392e3
Agustin Feijoo <agus99_feijoo@hotmail.com>
2022-10-06 18:33:49 -0300

    agregando APIS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React from 'react';
 import CartWidget from './componentes/CartWidget';
 import Producto from './componentes/itemListContainer/Productos/Producto'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import {  Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Inicio from '../src/ItemListContainer/Inicio'
 import Herreria from './ItemListContainer/herreria';
 import Electrodos from './electrodos';
@@ -16,44 +16,38 @@ import Carpinteria from './componentes/itemListContainer/Carpinteria'
 import ApiCarpinteria from './componentes/apis/ApiCarpinteria';
 
 
+const DefaultContainer=()=>(
+  <div>
+
+  </div>
+
+
+)
 
 
 
 function App() {
   return (
     <div className="App">
-       <Navbar> 
-            </Navbar>
-
-
-       <Router>
       
-      <Row style={{minHeight:"90vh"}}>
-          <Col lg={12} className={"margin-top"}>
-            <Routes>
-              
-              
-            <Route path="/Producto" exact element={<Producto></Producto>} />
-            <Route path="/Inicio" exact element={<Inicio></Inicio>}/>
-            <Route path='/Herreria' exact element={<Herreria></Herreria>}/>
-            <Route path='/Carpinteria' exact element={<Carpinteria></Carpinteria>}/>
-            <Route path='/Electrodos' exact element={<Electrodos></Electrodos>}/>
-            <Route path='/Alambre' exact element={<Alambre></Alambre>}/>
-            <Route path='/CartWidget' exact element={<CartWidget></CartWidget>}/>
-            <Route path='/Backend/API/Carpinteria' exact element={<ApiCarpinteria></ApiCarpinteria>}/>
+      <Router>
+            <Navbar />
+        <Routes>
+        <Route path="/Producto" exact element={<Producto></Producto>} />
+        <Route path="/Inicio" exact element={<Inicio></Inicio>}/>
+        <Route path='/Herreria' exact element={<Herreria></Herreria>}/>
+        <Route path='/Carpinteria' exact element={<Carpinteria></Carpinteria>}/>
+        <Route path='/Electrodos' exact element={<Electrodos></Electrodos>}/>
+        <Route path='/Alambre' exact element={<Alambre></Alambre>}/>
+        <Route path='/CartWidget' exact element={<CartWidget></CartWidget>}/>
+        </Routes>
+      </Router>
 
-     
 
-            </Routes>
-            </Col>
-            </Row>
-            </Router>
-            
-            
-   
-   
+
+
     </div>
-    
+
   )
 }
 
--- src/componentes/itemListContainer/Carpinteria.js
+++ src/componentes/itemListContainer/Carpinteria.js
@@ -1,6 +1,21 @@
+import { useEffect,useState } from "react";
+
 const Carpinteria=()=>{
+     let [datos,setDatos]=useState;
+    useEffect(()=>{
+        fetch("http://localhost:3001/ApiCarpinteria.json")
+        .then(response =>
+             response.json())
+        .then(datos=>console.log(datos))
+        setDatos(datos);
+    },[])
     
-    return(<div>asd</div>)
+    return(<div>
+        {datos?.foreach(producto=>{
+            console.log(producto.name)
+        return producto.name
+        })}
+    </div>)
 
 }
 export default Carpinteria
